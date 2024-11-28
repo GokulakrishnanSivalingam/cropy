b6da622e68f2c87d23255806d7f811978d1afda9
1234gk <144155567+1234gk@users.noreply.github.com>
2024-11-28 16:39:19 +0530

    adding mobile view

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,26 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import './Header.css';
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 
-const Header=()=>{
+const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
 
-return(
-    <header className="header">
-        <h1>Farm Fresh Products</h1>
-        <nav>
-            <ul className="nav-links">
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/">Products</Link></li>
-                <li><Link to="/contact">Contact</Link></li> 
-                <li><Link to="/register">Signup</Link></li>
-              <div className="login">  <li><Link to="/login">login</Link></li></div>
-            </ul>
-        </nav>
-    </header>
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
 
-
-);
-
+    return (
+        <header className="header">
+            <h1>F Products</h1>
+            <nav className="navbar">
+                <ul className={`nav-links ${isOpen ? 'active' : ''}`}>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/products">Products</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
+                    <li><Link to="/register">Signup</Link></li>
+                    <div className="login">
+                        <li><Link to="/login">Login</Link></li>
+                    </div>
+                </ul>
+                <div className="hamburger" onClick={toggleMenu}>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                </div>
+            </nav>
+        </header>
+    );
 };
 
 export default Header;
