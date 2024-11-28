88fa93ecdf503faa966b04a0d5422aef25c59381
1234gk <144155567+1234gk@users.noreply.github.com>
2024-11-28 13:44:46 +0530

    adding router

---
+++ src/Header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import './Header.css';
+import {Link} from "react-router-dom"
+
+const Header=()=>{
+
+return(
+    <header className="header">
+        <h1>Farm Fresh Products</h1>
+        <nav>
+            <ul className="nav-links">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/">Products</Link></li>
+                <li><Link to="/">Contact</Link></li> 
+                <li><Link to="/register">Signup</Link></li>
+              <div className="login">  <li><Link to="/login">login</Link></li></div>
+            </ul>
+        </nav>
+    </header>
+
+
+);
+
+};
+
+export default Header;
